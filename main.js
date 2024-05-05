6e0e8e5bc7f788a7af2e805d98e8f84fa0d6b568
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-05-05 10:50:53 +0200

    corretto

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 let griglia = document.getElementById("grid");
+let gameover = false;
+let punteggio = 0
+let bombe = [ 5, 8, 10, 13, 17, 20, 22, 24, 25, 27, 30 ]
 
 for (let i = 1; i <= 100; i++) {
 	
 	// Con Virtual-DOM e append
 	let elemento = creaQuadrato(i);
-
-	//Aggiungi un event-listener
-	// elemento.addEventListener("click", function() {
-	// 	console.log("cliccato", this); //in questo caso this = elemento
-	// 	this.classList.toggle("evidenziata");
-	// });
-
+	let bomba = bombaEsplosa();
+	let gameover = false;
+	let punteggio = 0
+	
 	griglia.append(elemento);
-
 }
 
 
@@ -27,9 +26,22 @@ function creaQuadrato(contenuto) {
 	square.addEventListener("click", function() {
 		console.log("cliccato", this); //in questo caso this = square
 		this.classList.toggle("evidenziata");
-        square.innerText = contenuto;
-	});
+        
+		punteggio++;
+		console.log ("Punteggio attuale: ", punteggio)
+	}, {once: true});
+
 
 	//restituisci il quadrato al chiamante
 	return square;
+}            
+
+function bombaEsplosa (contenuto) {
+	bombe.classList.add(bomba)
+
+	bomba.addEventListener("click", function() {
+		console.log("BOOM!")
+		alert("hai perso!") 
+	})
 }
+
